6244dadaf054f611e82f116dc1a4353443d7ea2d
Shivam Mamgain <shivamMg@users.noreply.github.com>
2018-04-04 13:40:01 +0530

    Fix bug: Backtracking not working correctly
    Maintain a stack of input indexes to backtrack to previous states.

--- RD/main.go
+++ RD/main.go
@@ -1,44 +1,51 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
+	"fmt"
+	"log"
 
 	t "github.com/shivammg/parsers/types"
 )
 
 // Parser represents a Recursive Descent parser.
 type Parser struct {
-	input     []string
-	backtrack int
-	current   int
-	rules     map[string]func() (*t.Tree, error)
+	input []string
+	// Maintains input indexes as you keep deriving productions.
+	// Used in backtracking to a previous input index.
+	stack []int
+	// Input's index where we're currently at.
+	current int
+	// map[non-terminal] -> production
+	rules map[string]func() (*t.Tree, error)
 }
 
 // NewParser returns a new Parser.
 func NewParser(input []string) *Parser {
 	return &Parser{
-		input: input,
-		// Input's index stored for backtracking.
-		backtrack: -1,
-		// Input's index where we're currently at.
+		input:   input,
+		stack:   []int{-1},
 		current: -1,
-		// map[non-terminal] -> production
-		rules: make(map[string]func() (*t.Tree, error)),
+		rules:   make(map[string]func() (*t.Tree, error)),
 	}
 }
 
-// Match matches symbol with the next token in input.
-func (p *Parser) Match(symbol string) bool {
+// Match matches terminal with the next token in input.
+func (p *Parser) Match(term string) bool {
 	if p.current >= len(p.input)-1 {
 		return false
 	}
 	p.current++
-	return symbol == p.input[p.current]
+	return term == p.input[p.current]
 }
 
 // Backtrack resets current to where it was after last derivation.
 func (p *Parser) Backtrack() {
-	p.current = p.backtrack
+	l := len(p.stack)
+	last := p.stack[l-1]
+	p.stack = p.stack[:l-1]
+	p.current = last
 }
 
 // Register registers a production function for a non-terminal.
@@ -52,7 +59,7 @@ func (p *Parser) Run(nonTerm string) (*t.Tree, error) {
 	if !ok {
 		return nil, errors.New("Rule does not exist")
 	}
-	p.backtrack = p.current
+	p.stack = append(p.stack, p.current)
 	tree, err := f()
 	if err != nil {
 		p.Backtrack()
@@ -61,4 +68,41 @@ func (p *Parser) Run(nonTerm string) (*t.Tree, error) {
 }
 
 func main() {
+	p := NewParser([]string{"a", "c"})
+
+	p.Register("E", func() (*t.Tree, error) {
+		if p.Match("a") {
+			t1, err := p.Run("F")
+			if err == nil {
+				return t.NewTree("E", t1), nil
+			}
+		}
+		p.Backtrack()
+		t1, err := p.Run("G")
+		if err == nil {
+			return t.NewTree("E", t1), nil
+		}
+		return nil, errors.New("No match")
+	})
+
+	p.Register("F", func() (*t.Tree, error) {
+		if p.Match("b") {
+			return t.NewTree("F", t.NewTree("b")), nil
+		}
+		return nil, errors.New("No match")
+	})
+
+	p.Register("G", func() (*t.Tree, error) {
+		if p.Match("c") {
+			return t.NewTree("F", t.NewTree("c")), nil
+		}
+		return nil, errors.New("No match")
+	})
+
+	tree, err := p.Run("E")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	b, _ := json.MarshalIndent(tree, "", "  ")
+	fmt.Println(string(b))
 }
